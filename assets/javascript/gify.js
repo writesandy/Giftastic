432cbcc26743dcddcdd20eab454db229109e9d50
writesandy <writesandy@gmail.com>
2018-03-22 18:36:24 -0500

    html page

---
+++ assets/javascript/gify.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+
+var topics = ["cinderella", "snow white", "princess leia", "pocahontas", "mulan" ]
+
+function displayGifies {
+    var gify = $(this).attr("")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
