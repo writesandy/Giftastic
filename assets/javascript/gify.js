9bcb936bf9667a1cc6e6bbaac12c57c6d33f6a97
writesandy <writesandy@gmail.com>
2018-03-23 19:49:45 -0500

    adds

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -2,15 +2,13 @@ $(document).ready(function() {
 
 
 
-
 let topics = ["cinderella", "snow+white", "princess+leia", "pocahontas", "mulan"]
 
 function displayGifies () {
     let gify = $(this).attr("data-name");
-    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cinderella + "&api_key=dc6zaTOxFJmzC";
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=cinderella&api_key=dc6zaTOxFJmzC";
 
-
-
+ 
 $.ajax ({
     url: queryURL,
     method: "GET"
