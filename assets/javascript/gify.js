b639e38645978278c0925798af3638bc820a5195
writesandy <writesandy@gmail.com>
2018-03-23 19:43:41 -0500

    update links

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -1,33 +1,28 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
 
-let topics = ["cinderella", "snow white", "princess leia", "pocahontas", "mulan" ]
+
+
+let topics = ["cinderella", "snow+white", "princess+leia", "pocahontas", "mulan"]
 
 function displayGifies () {
-    let gify = $(this).attr("");
-    const gifies = "http://api.giphy.com/v1/gifs/search?q=" + gify + "&api_key=dc6zaTOxFJmzC",
-}
+    let gify = $(this).attr("data-name");
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cinderella + "&api_key=dc6zaTOxFJmzC";
 
 
 
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
 }
+
+
+
+});
